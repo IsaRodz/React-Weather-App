2077c5aef88dc21f33cc1b383df2d6f0cf995cdd
IsaRodz <rodrigueziriarte96@gmail.com>
2020-05-10 14:19:29 -0500

    Updates

---
+++ precache-manifest.6848ee4ebbf3a48182b3da4c6041e331.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fa8ec3af44d7fc2d362e493c1b1eb6",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "b6a6698ecce121745855",
+    "url": "/react-weather-app/static/css/main.2b65a87f.chunk.css"
+  },
+  {
+    "revision": "03cc777546fdac6649d4",
+    "url": "/react-weather-app/static/js/2.06b208da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.06b208da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a6698ecce121745855",
+    "url": "/react-weather-app/static/js/main.a73ac90b.chunk.js"
+  },
+  {
+    "revision": "61e8186d19e7843cc21e",
+    "url": "/react-weather-app/static/js/runtime-main.f27a513d.js"
+  },
+  {
+    "revision": "5529cd968d0e07e74fe63d4a807812d3",
+    "url": "/react-weather-app/static/media/map.5529cd96.svg"
+  }
+]);
